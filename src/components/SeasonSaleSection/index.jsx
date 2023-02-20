259565c8c864f17e14ee674eca1c9b953dcbd93e
evgenii <gutenberg.eugen@gmail.com>
2023-02-20 11:19:28 +0100

    added animation Homepage

--- src/components/SeasonSaleSection/index.jsx
+++ src/components/SeasonSaleSection/index.jsx
@@ -2,18 +2,37 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import img_sale from './media/SeasonSale.png';
 import s from './style.module.sass';
+import { motion } from 'framer-motion';
 
 export default function SeasonSaleSection() {
+
+  const textAnimation = {
+    hidden: {
+        x: -1000,
+        opasity: 0,
+    },
+    visible: custom => ({
+        x: 0,
+        opasity: 1,
+        transition: {delay: custom * 0.2, duration: 0.5},
+    }),
+};
+
+
   return (
-    <div className={s.sale}>
+    <motion.div 
+      initial="hidden"
+      whileInView="visible" 
+      viewport={{ amount: 0.5, once: true }}
+      className={s.sale}>
         <div className={s.block_text}>
-            <h3>Sale</h3>
-            <p className={s.text_new_season}>the new season</p>
+            <motion.h3 custom={1} variants={textAnimation}>Sale</motion.h3>
+            <motion.p custom={2} variants={textAnimation} className={s.text_new_season}>the new season</motion.p>
             <Link to='/stock' className={s.button_promotion}>
-                <button className={s.promotions}>All promotions</button>    
+                <motion.button custom={3} variants={textAnimation} className={s.promotions}>All promotions</motion.button>    
             </Link>
         </div>
         <img src={img_sale} alt="img_sale" />
-    </div>
+    </motion.div>
   )
 }
